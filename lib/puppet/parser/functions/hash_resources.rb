94bb41862714f0123be6193ed634441b1153a020
Rob Kooper <kooper@illinois.edu>
2021-03-26 07:31:18 -0500

    bump version numbers

--- lib/puppet/parser/functions/hash_resources.rb
+++ lib/puppet/parser/functions/hash_resources.rb
@@ -2,7 +2,7 @@
 module Puppet::Parser::Functions
   newfunction(:hash_resources) do |args|
     hash = args[0]
-    raise Puppet::ParserError, "Must provide a hash" unless hash.is_a? Hash
+    raise Puppet::ParserError, 'Must provide a hash' unless hash.is_a? Hash
 
     Puppet::Parser::Functions.autoloader.load(:create_resources) \
       unless Puppet::Parser::Functions.autoloader.loaded?(:create_resources)
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe 'hash_resources' do
-  let(:params) do {
-    :resources => {
-      'file' => {
-        '/tmp/foo' => {'ensure'=> 'present', 'content'=> 'foo'},
-        '/tmp/bar' => {'ensure'=> 'present', 'content'=> 'bar'},
-      }
+  let(:params) do
+    {
+      :resources => {
+        'file' => {
+          '/tmp/foo' => { 'ensure' => 'present', 'content' => 'foo' },
+          '/tmp/bar' => { 'ensure' => 'present', 'content' => 'bar' },
+        },
+      },
     }
-  } end
+  end
 
-
-  it { should contain_file('/tmp/foo').with_content('foo') }
-  it { should contain_file('/tmp/bar').with_content('bar') }
+  it { is_expected.to contain_file('/tmp/foo').with_content('foo') }
+  it { is_expected.to contain_file('/tmp/bar').with_content('bar') }
 end
